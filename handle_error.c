e723caa87cb845033a9b6aa4321354244f3fbefb
cassius <cassiusbessa@gmail.com>
2024-02-11 20:22:49 -0300

    fix: :bug: fix leak errors

--- handle_error.c
+++ handle_error.c
@@ -12,23 +12,27 @@
 
 #include "push_swap.h"
 
-void	handle_error(t_circular_list *stack_a, void *stack_b)
+void handle_error(t_circular_list *stack_a, void *stack_b)
 {
-	write(2, "Error\n", 6);
-	destroy_circular(stack_a);
-	if (stack_b)
-		destroy_circular((t_circular_list *) stack_b);
-	exit (0);
+  write(2, "Error\n", 6);
+  destroy_circular(stack_a);
+  if (stack_b)
+    destroy_circular((t_circular_list *)stack_b);
+  exit(0);
 }
 
-void	handle_checker_error(t_circular_list *stack_a, void *stack_b, char *i)
+void handle_checker_error(t_circular_list *stack_a, void *stack_b, char *i)
 {
-	write(2, "Error\n", 6);
-	destroy_circular(stack_a);
-	if (stack_b)
-		destroy_circular((t_circular_list *) stack_b);
-	if (i)
-		free(i);
-	i = NULL;
-	exit (0);
+  write(2, "Error\n", 6);
+  destroy_circular(stack_a);
+  stack_a = NULL;
+  if (stack_b)
+  {
+    destroy_circular((t_circular_list *)stack_b);
+    stack_b = NULL;
+  }
+  if (i)
+    free(i);
+  i = NULL;
+  exit(0);
 }
